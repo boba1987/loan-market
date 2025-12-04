047a9c674ffc6211cc1958b51fea705d819c404b
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2025-12-04 14:31:23 +0100

    Add mock data

---
+++ src/loan_market/mock_data.clj
@@ -0,0 +1,22 @@
+(ns loan-market.mock-data)
+
+(def banks
+  [{:id 1
+    :name "Intesa San Paolo"
+    :interest 3.5}
+   {:id 2
+    :name "UniCredit"
+    :interest 2.8}
+   {:id 3
+    :name "AIK Bank"
+    :interest 4.2}
+   {:id 4
+    :name "Raiffeisen Bank"
+    :interest 3.1}
+   {:id 5
+    :name "Erste Bank"
+    :interest 3.9}
+   {:id 6
+    :name "OTP Bank"
+    :interest 2.5}])
+
