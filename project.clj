75ff8706fdc7defa006df03986987e1eddfac9b5
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2026-03-15 01:23:09 +0100

    Update project configuration: Change `criterium` dependency version in `project.clj`, remove unused `.idea` configuration files, and add a new bank entry to `banks.csv`.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.leiningen-test "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.12.4"]]
   :profiles {:dev {:dependencies [[midje "1.10.10"]
-                                  [criterium/criterium {:mvn/version "0.5.153"}]]
+                                  [criterium "0.5.153-ALPHA"]]
                    :plugins [[lein-midje "3.2.1"]]}})
